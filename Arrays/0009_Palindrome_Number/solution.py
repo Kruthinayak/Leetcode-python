f1aeaffe0044be4df73d7cc5686125ebe66d80f3
Kruthi <kruthinayaka07@gmail.com>
2026-07-19 23:26:02 +0530

    LeetCode #9 - Palindrome Number solution

--- Arrays/0009_Palindrome_Number/solution.py
+++ Arrays/0009_Palindrome_Number/solution.py
@@ -8,6 +8,6 @@ class Solution(object):
         while x:
             if x // div != x % 10: return False
             x=(x % div)// 10
-            div=div/100
+            div=div//100
         return True
         
